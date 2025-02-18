498fa37dd5cfb817b767a287e44d6c99c5ced411
mhlehri <lehrimirza101@gmail.com>
2025-02-18 22:50:39 +0600

    added empty ui to mybookings page

--- src/pages/MyBookings.tsx
+++ src/pages/MyBookings.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import {
   Table,
   TableBody,
@@ -10,6 +11,8 @@ import {
 import { useGetAllMyBookingsQuery } from "@/redux/features/bookings/bookingsApi";
 import { selectCurrentUser } from "@/redux/store";
 import { TBooking } from "@/types/booking";
+import { Calendar, X } from "lucide-react";
+import { Link } from "react-router-dom";
 
 export default function MyBookings() {
   const user = selectCurrentUser();
@@ -17,31 +20,41 @@ export default function MyBookings() {
   // console.log("my bookings data =>", data);
 
   return (
-    <div className="max-w-7xl mx-auto px-4 lg:px-0 py-12">
-      <h1 className="text-3xl font-bold mb-8">My Bookings</h1>
-      <Table>
-        <TableCaption>A list of your recent bookings</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Room Name</TableHead>
-            <TableHead>Date & Time</TableHead>
-            <TableHead>Status</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {data && !isLoading
-            ? data?.data.map((booking: TBooking) => (
+    <div className="max-w-7xl mx-auto px-4 lg:px-0 py-12 flex items-center justify-center h-[70vh]">
+      {data && !isLoading && data?.data.length > 0 ? (
+        <div>
+          <h1 className="text-3xl font-bold mb-8">My Bookings</h1>
+          <Table>
+            <TableCaption>A list of your recent bookings</TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Room Name</TableHead>
+                <TableHead>Date & Time</TableHead>
+                <TableHead>Status</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {data?.data.map((booking: TBooking) => (
                 <TableRow key={booking._id}>
                   <TableCell>{booking.room.name}</TableCell>
                   <TableCell>{booking.date.toLocaleString()}</TableCell>
                   <TableCell>{booking.isConfirmed}</TableCell>
                 </TableRow>
-              ))
-            : isLoading
-            ? "loading..."
-            : "No bookings yet"}
-        </TableBody>
-      </Table>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+      ) : (
+        <div className="text-gray-500 my-4 text-2xl text-center">
+          <X className="mx-auto size-40" />
+          <h6> No bookings yet</h6>
+          <Button className="mt-4" variant="outline" asChild>
+            <Link to="/meeting-rooms" className="text-indigo-600">
+              <Calendar className="mx-auto size-4" /> Book a room
+            </Link>
+          </Button>
+        </div>
+      )}
     </div>
   );
 }
